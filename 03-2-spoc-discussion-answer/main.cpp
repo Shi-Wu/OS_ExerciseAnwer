d3da8e89c385457b8e7ceb84502a297c393c2574
Albert13 <qq1473474689@163.com>
2016-03-09 16:56:28 +0800

    update

---
+++ 03-2-spoc-discussion-answer/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+void load(){}
+
+void getContent1(int num){
+
+}
+
+void getContent2(int num){
+
+}
+
+void getValue(int num){
+
+}
+
+int main(){
+	load();
+
+	
+	int vd , hi, mi, lo; 
+
+	cin>>hex>>vd;
+
+	hi = vd >> 10;
+	mi = (vd & 992) >> 5;
+	lw = vd & 31;
+
+	getContent1(hi);
+
+	getContent2(mi);
+
+	getValue(lw);
+
+	return 0;
+
+}
