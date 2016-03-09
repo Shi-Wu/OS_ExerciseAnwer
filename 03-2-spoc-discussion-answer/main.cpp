fae816521fc8483d620fc38f510cf69889ea64d9
Albert13 <qq1473474689@163.com>
2016-03-09 19:01:19 +0800

    update

--- 03-2-spoc-discussion-answer/main.cpp
+++ 03-2-spoc-discussion-answer/main.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
 #include <cstring>
-
-
+#include <fstream>
 using namespace std;
-
-void load(){}
-
-void getContent1(int num){
-
+int mem[128][32];
+void load(string inTxt){
+	ifstream in(inTxt.c_str());
+	int temp;
+	for(int i = 0 ; i < 128 ; i++){
+		for(int j = 0 ; j < 32 ; j++){
+			in >> hex >> temp;
+			mem[i][j] = temp;
+		}
+	}
 }
-
-void getContent2(int num){
-
+int getContent1(int offset){
+	int val = mem[0x11][offset];
+	if (val >> 7 == 0){
+		return -1;
+	}
+	return val&127;
 }
-
-void getValue(int num){
-
+int getContent2(int num , int offset){
+	if (num == -1){
+		return -1;
+	}
+	int val = mem[num][offset];
+	if (val >> 7 == 0){
+		return -1;
+	}
+	return val&127;
 }
-
+int getValue(int num,  int offset){
+	if (num == -1){
+		cout <<"fault"<<endl;
+		return -1;
+	}
+	int val = mem[num][offset];
+	cout <<"the val is " << "0x" <<hex<<val << endl;
+	return val;
+}
 int main(){
-	load();
-
-	
+	load("in.txt");
 	int vd , hi, mi, lo; 
-
-	cin>>hex>>vd;
-
-	hi = vd >> 10;
-	mi = (vd & 992) >> 5;
-	lw = vd & 31;
-
-	getContent1(hi);
-
-	getContent2(mi);
-
-	getValue(lw);
-
+	while(true){
+		cout << "please input:";
+		cin>>hex>>vd;
+		hi = vd >> 10;
+		mi = (vd & 992) >> 5;
+		lo = vd & 31;	
+		getValue( getContent2( getContent1(hi) , mi), lo);
+	}
 	return 0;
-
 }
